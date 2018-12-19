cd7ecca88cba4136ac76cdd34b130af775311ffa
a <b>
2018-12-19 21:07:14 +0900

    3

---
+++ teamProject/src/main/java/com/team/spring/HomeController2.java
@@ -0,0 +1,24 @@
+package com.team.spring;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HomeController2 {
+	
+	@RequestMapping("home")
+	public ModelAndView home() {	
+
+
+		ModelAndView mView=new ModelAndView();
+			mView.setViewName("home");	
+
+		return mView;
+	}
+	
+}
+
