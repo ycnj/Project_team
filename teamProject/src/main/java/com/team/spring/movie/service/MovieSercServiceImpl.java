22bf61bb946c8136d9a07d78dd39a804d515dc56
acorn <acorn@acorn-PC>
2018-12-26 12:09:49 +0900

    dd

--- teamProject/src/main/java/com/team/spring/movie/service/MovieSercServiceImpl.java
+++ teamProject/src/main/java/com/team/spring/movie/service/MovieSercServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.json.simple.ItemList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.servlet.ModelAndView;
